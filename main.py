a2167570625ac4d7f1f354f599dceac12364ef23
ibautista007 <anime.mxa@gmail.com>
2019-07-02 20:30:01 -0700

    Actualizacion de main.py
    Se crea control para determinar la cantidad de cuerdas y determina proceso para hacer entrega de valores de los costos por union de cuerdas

--- main.py
+++ main.py
@@ -1,23 +1,50 @@
+ropes = []
+num = int(input('Ingrese el numero de cuerdas que conectará:'))
+print ('Ingrese el tamaño: ')
+for i in range(int(num)):
+   n = input("num :")
+   ropes.append(int(n))
 
 
-ropes = [4,3,2,6] 
+#ropes = [4,3,2,6]
 ropes.sort()
-res = 0
-primerd = ropes[0]
-segundod = ropes[1]
-suma = primerd + segundod
-pair = 0
-i = 2
+
+
 lenght = len(ropes)
 
-while i < lenght: 
-  res += suma
-  pair = suma + ropes[i]
-  suma = pair
-  i +=1
-
-
-print ("Total de costo por union ",res + suma)
+if lenght == 1:
+ print ("Solo se tiene una cuerda no se puede tomar calor de costo por union")
+elif lenght == 2:
+ res = 0
+ primerd = ropes[0]
+ segundod = ropes[1]
+ suma = primerd + segundod
+ pair = 0
+ print ("Total de costo por union: ",suma)
+elif lenght == 3:
+ res = 0
+ primerd = ropes[0]
+ segundod = ropes[1]
+ suma = primerd + segundod
+ pair = 0
+ i = 2
+  while i < lenght:
+   res = suma + ropes[i]
+   i +=1
+ print ("Total de costo por union ",res)
+else:
+ res = 0
+ primerd = ropes[0]
+ segundod = ropes[1]
+ suma = primerd + segundod
+ pair = 0
+ i = 2
+  while i < lenght:
+   res += suma
+   pair = suma + ropes[i]
+   suma = pair
+   i +=1
+ print ("Total de costo por union ",res+suma)
 
 x = len(ropes)
 
